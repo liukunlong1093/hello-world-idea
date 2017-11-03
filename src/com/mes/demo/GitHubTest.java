702583c9fb4bad6864fc9b7a4cd04b9e6e079c91
liukunlong1093 <liukunlong1093@163.com>
2017-11-03 17:07:48 +0800

    测试列表提交

--- src/com/mes/demo/GitHubTest.java
+++ src/com/mes/demo/GitHubTest.java
@@ -2,9 +2,12 @@ package com.mes.demo;
 
 /**
  * GitHub提交测试类
+ * IntelliJ IDEA
  */
 public class GitHubTest {
     public static void main(String[] args){
         System.out.println("GitHub! Test!33333333333333333333");
+        String msg="Test";
+        System.out.println("-----------ftdsoft.com-----------值=" +msg+ "," + "当前类=GitHubTest.main()");
     }
 }
--- src/com/mes/demo/HelloWorld.java
+++ src/com/mes/demo/HelloWorld.java
@@ -5,6 +5,11 @@ package com.mes.demo;
  */
 public class HelloWorld {
     public  static void main(String[] args){
+        Demo demo=new Demo();
+        demo.setId("1");
+        demo.setName("小花");
         System.out.println("我爱你：HelloWorld");
+        System.out.println("-----------ftdsoft.com-----------值=" +demo.toString()+ "," + "当前类=HelloWorld.main()");
+
     }
 }
