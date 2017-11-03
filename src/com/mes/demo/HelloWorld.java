309a1919eac236001c296e0465be8030ff21437b
liukunlong1093 <liukunlong1093@163.com>
2017-11-03 14:22:02 +0800

    初始化提交

---
+++ src/com/mes/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.mes.demo;
+
+/**
+ * HelloWorld IDEA 测试类
+ */
+public class HelloWorld {
+    public  static void main(String[] args){
+        System.out.println("我爱你：HelloWorld");
+    }
+}
