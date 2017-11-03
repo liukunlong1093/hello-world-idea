8b3c08c95255494530351bbfec640e09e1fe2536
liukunlong1093 <liukunlong1093@163.com>
2017-11-03 14:23:58 +0800

    初始化提交Test

--- src/com/mes/demo/HelloWorld.java
+++ src/com/mes/demo/HelloWorld.java
@@ -6,5 +6,6 @@ package com.mes.demo;
 public class HelloWorld {
     public  static void main(String[] args){
         System.out.println("我爱你：HelloWorld");
+        System.out.println("Test GitHub!");
     }
 }
