578439048bcfdeba18e53355d392f30a676ec9d4
liukunlong1093 <liukunlong1093@163.com>
2017-11-06 15:05:00 +0800

    增加设计模式之代理模式代码

--- src/com/mes/structure/proxy/ReallySubject.java
+++ src/com/mes/structure/proxy/ReallySubject.java
@@ -3,7 +3,7 @@ package com.mes.structure.proxy;
 public class ReallySubject implements Subject {
     @Override
     public void request() {
-        String msg="发起的请求";
+        String msg="发起的请求.";
         System.out.println("-----------ftdsoft.com-----------值=" + msg+ "," + "当前类=ReallySubject.request()");
     }
 }
